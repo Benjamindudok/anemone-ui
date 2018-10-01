479aa68e2b499d0737dc71a18b2291ef9aeb09ca
Benjamin Dudok <BenjaminWD@gmail.com>
2018-10-01 18:21:08 +0200

    Simulate actual package use in test application

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,7 @@
 import Vue from 'vue';
-
-import Base from './components/base/base.vue';
-import HelloWorld from './components/hello-world/hello-world.vue';
-import Overlay from './components/overlay/overlay.vue';
-import Transitions from './components/transitions/transitions.vue';
-import { convertToSnakeCase } from './utils/stringUtils';
 import App from './views/app.vue';
 
-const components: any = {
-    Base,
-    HelloWorld,
-    Overlay,
-    Transitions,
-};
-
-// automatically register all components
-Object.keys(components).forEach((name: string) =>
-{
-    Vue.component('anemone-' + convertToSnakeCase(name), components[name]);
-});
+import './package';
 
 Vue.config.productionTip = false;
 
--- src/package.ts
+++ src/package.ts
@@ -1,22 +1,29 @@
 import Vue from 'vue';
 
-import AUIBase from './components/base/base.vue';
-import AUIHelloWorld from './components/hello-world/hello-world.vue';
-import AUIOverlay from './components/overlay/overlay.vue';
+import Base from './components/base/base.vue';
+import HelloWorld from './components/hello-world/hello-world.vue';
+import Overlay from './components/overlay/overlay.vue';
+import Transitions from './components/transitions/transitions.vue';
+import { convertToSnakeCase } from './utils/stringUtils';
 
 const components: any = {
-    AUIBase,
-    AUIHelloWorld,
-    AUIOverlay
+    Base,
+    HelloWorld,
+    Overlay,
+    Transitions,
 };
+
 // automatically register all components
 Object.keys(components).forEach((name: string) =>
 {
-    Vue.component(name, components[name]);
+    Vue.component('anemone-' + convertToSnakeCase(name), components[name]);
 });
 
 // export components individually,
 // incase users want to assign custom html tags
-export const helloWorld: Vue = components.AnemonuiHelloWorld;
+export const base: Vue = components.Base;
+export const helloWorld: Vue = components.HelloWorld;
+export const overlay: Vue = components.Overlay;
+export const transitions: Vue = components.Transitions;
 
 export default components;
