bbc7944bf8e8b0132846b41d998b9d150526c730
Benjamin Dudok <BenjaminWD@gmail.com>
2018-10-06 15:37:39 +0200

    Don't explicitly check variable names

--- src/utils/stringUtils.ts
+++ src/utils/stringUtils.ts
@@ -1,8 +1,8 @@
 // convert PascalCase to snake-case as found on:
 // https://stackoverflow.com/questions/30521224/javascript-convert-pascalcase-to-underscore-case
-export const convertToSnakeCase: any = (string: string): string =>
+export const convertToSnakeCase: any = (original: string): string =>
 {
-    return string.replace(/\.?([A-Z]+)/g, (x: any, y: any): any =>
+    return original.replace(/\.?([A-Z]+)/g, (x: any, y: any): any =>
     {
         return '-' + y.toLowerCase();
     }).replace(/^-/, '');
